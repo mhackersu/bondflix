5cd59cb46c9bf501cc12f9d61cc527a1da4700e2
Michael Neal <iammichaelneal@gmail.com>
2018-01-18 15:09:45 -0600

    props.color

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,16 +1,20 @@
 var ce = React.createElement;
 
 var TheTitle = function(props) {
-	return ce('div', null, ce('h1', null, props.title));
+	return ce(
+		'div',
+		null,
+		ce('h1', { style: { color: props.color } }, props.title)
+	);
 };
 
 var FirstComponent = function() {
 	return ce(
 		'div',
 		null,
-		ce(TheTitle, { title: 'Casino Royale' }),
-		ce(TheTitle, { title: 'Dr. No' }),
-		ce(TheTitle, { title: 'From Russia with Love' })
+		ce(TheTitle, { color: 'indigo', title: 'Casino Royale' }),
+		ce(TheTitle, { color: 'indigo', title: 'Dr. No' }),
+		ce(TheTitle, { color: 'indigo', title: 'From Russia with Love' })
 	);
 };
 
