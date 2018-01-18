bbac32e17b114a0a14867b14f3f595d5c50d6f03
Michael Neal <iammichaelneal@gmail.com>
2018-01-18 14:58:26 -0600

    title component

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,12 +1,20 @@
+var Title = function() {
+	return React.createElement(
+		'div',
+		null,
+		React.createElement('h1', null, 'Title Component')
+	);
+};
+
 var FirstComponent = function() {
 	return React.createElement(
-		"div",
+		'div',
 		null,
-		React.createElement("h1", null, "First Compoment.")
+		React.createElement('h1', null, 'First Compoment.')
 	);
 };
 
 ReactDOM.render(
 	React.createElement(FirstComponent),
-	document.getElementById("app")
+	document.getElementById('app')
 );
