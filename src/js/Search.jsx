c0674692a42a1c054669018011bdcc4a9bfb059e
Michael Neal <iammichaelneal@gmail.com>
2018-01-20 15:26:35 -0600

    rendering titles

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import preload from '../../data.json';
 
 const Search = () => (
-	<div className="search">
-		<pre>
-			<code>{JSON.stringify(preload, null, 4)}</code>
-		</pre>
-	</div>
+	<div className="search">{preload.collection007.map(collection => <h3>{collection.title}</h3>)}</div>
 );
 
 export default Search;
