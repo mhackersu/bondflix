b849d4fb9a4c0c979b01ba3a091da650cdba207e
Michael Neal <iammichaelneal@gmail.com>
2018-01-20 13:22:19 -0600

    client side 404

--- src/js/ClientApp.jsx
+++ src/js/ClientApp.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 
 import '../styles/app.sass';
 
+const FourOhFour = () => <h1>404</h1>;
+
 const App = () => (
 	<BrowserRouter>
 		<div className="app">
-			<Route exact path="/" component={Landing} />
-			<Route path="/search" component={Search} />
+			<Switch>
+				<Route exact path="/" component={Landing} />
+				<Route path="/search" component={Search} />
+				<Route component={FourOhFour} />
+			</Switch>
 		</div>
 	</BrowserRouter>
 );
