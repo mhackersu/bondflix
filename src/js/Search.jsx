fb45a3793d69f5db4534c32672f83f6db2488c77
Michael Neal <iammichaelneal@gmail.com>
2018-01-20 15:46:35 -0600

    loading titles

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import preload from '../../data.json';
 
 const Search = () => (
-	<div className="search">{preload.collection007.map(collection => <h3>{collection.title}</h3>)}</div>
+	<div className="search">
+		{preload.collection007.map(collection => (
+			<div className="collection-card">
+				<img alt={`${collection.title} Collection Poster`} src={`${collection.poster}`} />
+				<div>
+					<h3>{collection.title}</h3>
+					<p>{`Budget ${collection.budget}`}</p>
+					<p>{`Released ${collection.released}`}</p>
+				</div>
+			</div>
+		))}
+	</div>
 );
 
 export default Search;
