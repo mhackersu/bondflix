3ac5b4c09fdc4e6092c957956c4346d9a436f36c
Michael Neal <iammichaelneal@gmail.com>
2018-01-20 16:03:01 -0600

    collection={collection}

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -3,7 +3,7 @@ import CollectionCard from './CollectionCard';
 import preload from '../../data.json';
 
 const Search = () => (
-	<div className="search">{preload.collection007.map(collection => <CollectionCard collection={show} />)}</div>
+	<div className="search">{preload.collection007.map(collection => <CollectionCard collection={collection} />)}</div>
 );
 
 export default Search;
