b59959436d9c3fca1222249f0940e15e1a2f1218
Michael Neal <iammichaelneal@gmail.com>
2018-01-22 17:24:57 -0600

    styled component

--- src/js/CollectionCard.jsx
+++ src/js/CollectionCard.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { string } from 'prop-types';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+	width: 32%;
+	border: 2px solid #333;
+	border-radius: 4px;
+	margin-bottom: 25px;
+	padding-right: 10px;
+	overflow: hidden;
+`;
+
+const Image = styled.img`
+	width: 46%;
+	float: left;
+	margin-right: 10px;
+`;
+
 const CollectionCard = props => (
 	<Wrapper>
 		<Image
