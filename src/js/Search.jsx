26b0836ca31e628c337203ada365ac2f165d51a6
Michael Neal <iammichaelneal@gmail.com>
2018-01-23 15:18:44 -0600

    es6 class component for search

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CollectionCard from './CollectionCard';
 import preload from '../../data.json';
 
-const Search = () => (
-	<div className="search">
-		<div>
-			{preload.collection007.map(collection => (
-				<CollectionCard key={collection.imdb_id} {...collection} />
-			))}
-		</div>
-	</div>
-);
+class Search extends Component {
+	constructor (props) {
+		super(props)
+
+		this.state = {
+			searchTerm: 'Debug Statement'
+		}
+	}
+	render() {
+		return (
+			<div className="search">
+				<h1>BondFlix</h1>
+				<input type="text" placeholder="Search" />
+				<div>
+					{preload.collection007.map(collection => (
+						<CollectionCard key={collection.imdb_id} {...collection} />
+					))}
+				</div>
+			</div>
+		);
+	};
+};
 
 export default Search;
