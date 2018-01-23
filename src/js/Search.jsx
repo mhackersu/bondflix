e2dbba0bd8fa7e4c5418efd939e813babbcae165
Michael Neal <iammichaelneal@gmail.com>
2018-01-23 15:30:20 -0600

    .bind(this)

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -3,26 +3,41 @@ import CollectionCard from './CollectionCard';
 import preload from '../../data.json';
 
 class Search extends Component {
-	constructor (props) {
-		super(props)
+	constructor(props) {
+		super(props);
 
 		this.state = {
 			searchTerm: 'Debug Statement'
-		}
+		};
+
+		this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
 	}
+
+	handleSearchTermChange(event) {
+		this.setState({ searchTerm: event.target.value });
+	}
+
 	render() {
 		return (
 			<div className="search">
 				<h1>BondFlix</h1>
-				<input type="text" placeholder="Search" />
+				<input
+					onChange={this.handleSearchTermChange}
+					value={this.state.searchTerm}
+					type="text"
+					placeholder="Search"
+				/>
 				<div>
 					{preload.collection007.map(collection => (
-						<CollectionCard key={collection.imdb_id} {...collection} />
+						<CollectionCard
+							key={collection.imdb_id}
+							{...collection}
+						/>
 					))}
 				</div>
 			</div>
 		);
-	};
-};
+	}
+}
 
 export default Search;
