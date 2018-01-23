59ceae2a2d07d2f911fcf1d28d3e4151a649c3c0
Michael Neal <iammichaelneal@gmail.com>
2018-01-23 15:41:45 -0600

    babel-eslint

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -3,20 +3,12 @@ import CollectionCard from './CollectionCard';
 import preload from '../../data.json';
 
 class Search extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			searchTerm: 'Debug Statement'
-		};
-
-		this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
-	}
-
-	handleSearchTermChange(event) {
+	state = {
+		searchTerm: ''
+	};
+	handleSearchTermChange = event => {
 		this.setState({ searchTerm: event.target.value });
-	}
-
+	};
 	render() {
 		return (
 			<div className="search">
