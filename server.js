5483cd951dbd13628c571da871931a7969aa65da
Michael Neal <iammichaelneal@gmail.com>
2018-01-24 19:05:23 -0600

    building the dev server

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+module.exports = {
+	app() {
+		const app = express();
+		const indexPath = path.join(__dirname, 'index.html');
+		const publicPath = express.static(path.join(__dirname, 'public'));
+
+		app.use('/public', publicPath);
+		app.get('/', (_, res) => {
+			res.sendFile(indexPath);
+		});
+
+		return app;
+	}
+};
