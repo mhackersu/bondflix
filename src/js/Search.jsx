a941af15d03586038762dd473480ca682ec42f18
Mike Hacker <mhackersu@gmail.com>
2024-06-09 16:52:24 -0500

    more eslint

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -1,52 +1,44 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import CollectionCard from './CollectionCard';
-import preload from '../../data.json';
 
-class Search extends Component {
+class Search extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { searchTerm: '' };
 	}
 
-	handleSearchTermChange = (event) => {
+	handleChange = (event) => {
 		this.setState({ searchTerm: event.target.value });
 	};
 
+	filterCollections = () => {
+		const { searchTerm } = this.state; // Destructure state
+		return this.props.collections.filter(
+			(collection) =>
+				`${collection.title} ${collection.plot}`
+					.toUpperCase()
+					.indexOf(searchTerm.toUpperCase()) >= 0,
+		);
+	};
+
 	render() {
+		const { searchTerm } = this.state; // Destructure state for rendering
 		return (
-			<div className="search">
-				<div className="header">
-					<input
-						className="element-input-search"
-						onChange={this.handleSearchTermChange}
-						value={this.state.searchTerm}
-						type="text"
-						placeholder="Search"
-					/>
-					<Link className="search-title-bar" to="/">
-						BondFlix
-					</Link>
-				</div>
-				<div className="container-results">
-					<div className="results-search">
-						{preload.collection007.filter((
-							collection =>
-							`${collection.title} ${collection.plot}`.toUpperCase()
-							.indexOf(this.state.searchTerm.toUpperCase()) >= 0)
-							.map(collection => (
-								<CollectionCard 
-									key={collection.imdb_id} 
-									title={collection.title} 
-									genre={collection.genre} 
-									year={collection.year} 
-									poster={collection.poster} 
-									rating={collection.rating} 
-									votes={collection.votes} 
-									director={collection.director}
-								/>
-							)))}
-					</div>
+			<div>
+				<input type='text' onChange={this.handleChange} />
+				<div>
+					{this.filterCollections().map((collection) => (
+						<CollectionCard
+							key={collection.imdb_id}
+							title={collection.title}
+							genre={collection.genre}
+							year={collection.year}
+							poster={collection.poster}
+							rating={collection.rating}
+							votes={collection.votes}
+							director={collection.director}
+						/>
+					))}
 				</div>
 			</div>
 		);
