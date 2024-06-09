e844bc3fcedfac6c82df7f4da08f94f07b78bca5
Mike Hacker <mhackersu@gmail.com>
2024-06-09 16:35:25 -0500

    eslint cleanup

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -4,9 +4,10 @@ import CollectionCard from './CollectionCard';
 import preload from '../../data.json';
 
 class Search extends Component {
-	state = {
-		searchTerm: '',
-	};
+	constructor(props) {
+		super(props);
+		this.state = { searchTerm: '' };
+	}
 
 	handleSearchTermChange = (event) => {
 		this.setState({ searchTerm: event.target.value });
@@ -29,11 +30,19 @@ class Search extends Component {
 				</div>
 				<div className="container-results">
 					<div className="results-search">
-						{preload.collection007
-							.filter
-								(collection =>`${collection.title} ${collection.plot}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
-							.map
-								(collection => (<CollectionCard key={collection.imdb_id}{...collection}/>))}
+						{preload.collection007.filter((collection =>
+							`${collection.title} ${collection.plot}`.toUpperCase()
+							.indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+							.map(collection =>(<CollectionCard 
+							  key={collection.imdb_id} 
+							  title={collection.title} 
+							  genre={collection.genre} 
+							  year={collection.year} 
+							  poster={collection.poster} 
+							  rating={collection.rating} 
+							  votes={collection.votes} 
+							  director={collection.director} />))
+						}
 					</div>
 				</div>
 			</div>
