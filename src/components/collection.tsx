a37ba7a240d830d0d186c0ff92dc7c98d93cf8ac
Mike Hacker <mhackersu@gmail.com>
2024-06-12 11:42:16 -0500

    debugging destructuring

--- src/components/collection.tsx
+++ src/components/collection.tsx
@@ -1,18 +1,43 @@
-export const Collection = () => {
-	const data = getCollection();
-	return(
-		<div>
-			<p>hello collection</p>
-			<p>{data.title}</p>
-		</div>
-	);
-};
+import collection_data from '../data.json';
+
+import React, { Component } from 'react';
+
+// export const Collection = () => {
+// 	// const data = getCollection();
+// 	return(
+// 		<div>
+// 			<p>hello collection</p>
+// 			{/*<p>{data.title}</p>*/}
+// 		</div>
+// 	);
+// };
 
 const getCollection = () => {
-	// const data = '../data.json';
-	const data = {
-		title: 'data collection',
-	};
+	// const data = {
+	// 	title: 'data collection',
+	// };
 
-	return data;
+	// return data;
+	// return mydata;
+}
+
+export class Collection extends React.Component {
+    render() {
+        const { data } = this.props.collection_data;
+        return (
+            // <div>
+            //     {
+            //       Object.keys(data).map((e, i) => {
+            //         <SomeComponent key={i} {...e} />
+            //       })
+            //     }
+            // </div>
+			<div>
+			  {this.props.collection_data.data.map((e, i) => 
+			    // <SomeComponent key={i} item={e} />
+			    <SomeComponent key={i} label={e.label} value={e.value} />
+			  )}
+			</div>
+        )
+    }
 }
