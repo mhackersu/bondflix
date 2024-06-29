81d2c686d269acc720a9a2c41ca65de966b2695f
Mike Hacker <mhackersu@gmail.com>
2024-06-29 15:39:02 -0500

    migrate landing page

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,27 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
-      <h1>Vite + React</h1>
+      <h1>Bondflix</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        {/* <button onClick={() => setCount((count) => count + 1)}> */}
+        <button>
+          {/* count is {count} */}
+          View the gallery
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {/* Edit <code>src/App.tsx</code> and save to test HMR */}
+          Bondflix Collection
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the gallery button to view the collection
       </p>
     </>
   )
